c011443237c841c2782d4a453c37c6d60a2f772a
Alan Yuen <alanyuen95@gmail.com>
2019-03-12 23:57:23 -0700

    Basic lang sim experimental setup

--- attentionRNN.py
+++ attentionRNN.py
@@ -1,8 +1,152 @@
 import torch
 from torch import nn
-from torch.autograd import Variable
+if torch.cuda.is_available(): import torch.cuda as t
+else: import torch as t
 import torch.nn.functional as F
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+import math, random
+
+class EncoderRNN(nn.Module):
+    def __init__(self,  input_size, embed_size, hidden_size, 
+                        n_layers=1, dropout=0.5, bidirectional = True):
+        
+        super(EncoderRNN, self).__init__()
+        
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.embed_size = embed_size
+        
+        self.n_layers = n_layers
+        self.dropout = dropout
+        
+        self.embedding = nn.Embedding(input_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, n_layers, dropout=self.dropout, bidirectional=bidirectional)
+
+    def forward(self, input_seq, hidden = None):
+        if(len(input_seq) == 0):
+          return t.FloatTensor(1,input_seq.shape[1], self.hidden_size).fill_(0), None #initial h0
+        
+        embedded = self.embedding(input_seq) #[T,B,E]
+        outputs, hidden = self.lstm(embedded, hidden) #outputs : (T, B, (2*)H)
+        outputs = outputs[:, :, :self.hidden_size] + outputs[:, :, self.hidden_size:]  # Sum bidirectional outputs
+        return outputs, hidden
+      
+
+class Attn(nn.Module):
+    def __init__(self, method, hidden_size, g_size = int()):
+        '''
+        :param g_size:
+          goal vector length (assuming 1D)
+        '''
+        super(Attn, self).__init__()
+        self.method = method
+        self.hidden_size = hidden_size
+        self.g_size = g_size
+        self.attn = nn.Linear(self.hidden_size + g_size, hidden_size)
+        self.v = nn.Parameter(t.FloatTensor(hidden_size).uniform_(0,1))
+        stdv = 1. / math.sqrt(self.v.size(0))
+        self.v.data.normal_(mean=0, std=stdv)
+
+    def forward(self, encoder_outputs, g = None):
+        '''
+        :param hidden: 
+            previous hidden state of the decoder, in shape (layers*directions,B,H)
+        :param encoder_outputs:
+            encoder outputs from Encoder, in shape (T,B,H)
+        :param src_len:
+            used for masking. NoneType or tensor in shape (B) indicating sequence length
+        :return
+            attention energies in shape (B,T)
+        '''
+        max_len = encoder_outputs.size(0)
+        this_batch_size = encoder_outputs.size(1)
+        encoder_outputs = encoder_outputs.transpose(0,1) # [B*T*H]
+        attn_energies = self.score(encoder_outputs, g) # compute attention score
+        
+        return F.softmax(attn_energies, dim = 1).unsqueeze(1) # normalize with softmax #[B,1,T] 
+
+    def score(self, encoder_outputs, g = None):
+        if(self.g_size != 0): #g:(B,G)
+          g = g.float().repeat(encoder_outputs.shape[1],1,1).transpose(1,0) #(B,G) => (T,B,G) => (B,T,G)
+          inp = torch.cat((encoder_outputs, g),2) #[B,T,H] <concat> [B,T,G] ==> [B,T,(H+G)]
+        else:
+          inp = encoder_outputs
+          
+        energy = self.attn(inp)
+        energy = energy.transpose(2,1) # [B*H*T]
+        v = self.v.repeat(encoder_outputs.data.shape[0],1).unsqueeze(1) #[B*1*H]
+        energy = torch.bmm(v, energy) # [B*1*T]
+        return energy.squeeze(1) #[B*T]
+
+class AttnDecoder(nn.Module):
+    def __init__(self, layer_dims, g_size = int(), activation_fn = lambda a: a):
+        '''
+        :param g_size:
+          goal vector length (assuming 1D)
+        :param layer_dims:
+            [input size + g size, first hidden layer size, ..., last hidden layer size, output size]
+            input size : H
+            g size : V
+            output size : P(HONEMES_INVENTORY_SIZE)
+        '''
+        super(AttnDecoder, self).__init__()
+        #self.attn = attn # [A] -> B
+        
+        assert(len(layer_dims) > 1)
+        layer_dims[0] += g_size
+        self.input_size = layer_dims[0] #[H(+G), ...]
+        self.g_size = g_size
+        self.output_size = layer_dims[-1] #[... ,P]
+        
+        #self.ff = ff # B -> C
+        self.hidden_layers = nn.ModuleList([nn.Linear(layer_dims[i],layer_dims[i+1]) for i in range(len(layer_dims)-1)])
+        self.activation_fn = activation_fn
+        
+
+    def forward(self, encoder_outputs, attn_weights, g = None, debug = False):
+        '''
+        :param word_input:
+            word input for current time step, in shape (B)
+        :param last_hidden:
+            last hidden stat of the decoder, in shape (layers*direction*B*H)
+        :param encoder_outputs:
+            encoder outputs in shape (T*B*H)
+        :param attn_weights:
+            softmax'd attention weights from attn(encoder_outputs) in shape [B,1,T]
+        :return
+            decoder output (a_distro) in shape [B,P]
+        
+        Note: we run this one step at a time i.e. you should use a outer loop 
+            to process the whole sequence
+        Tip(update):
+        EncoderRNN may be bidirectional or have multiple layers, so the shape of hidden states can be 
+        different from that of DecoderRNN
+        You may have to manually guarantee that they have the same dimension outside this function,
+        e.g, select the encoder hidden state of the foward/backward pass.
+        '''
+        
+        #attn_weights = self.attn(encoder_outputs) #[B,1,T] 
+        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))  # [B,1,T].bmm([B,T,H]) ==> (B,1,H)
+        context = context.transpose(0, 1).squeeze(0)  # (B,H)
+        
+        if(self.g_size != 0): #g:(B,G)
+          inp = torch.cat((context, g),1) #(B,H+G)
+        else:
+          inp = context
+        
+        #return self.ff(context)  #feed-forward neural net [H,H',H'', ... ,P(HONEME_INVENTORY_SIZE)]
+        x = self.activation_fn(self.hidden_layers[0](inp))
+        for i in range(1,len(self.hidden_layers)-1):
+          x = self.activation_fn(self.hidden_layers[i](x))
+        x = self.hidden_layers[-1](x)
+        
+        if(debug): print(x)
+          
+        return F.softmax(x, dim = 1) #[B,P] -- a_distro for each batch item\
+        
+"""
 class EncoderRNN(nn.Module):
     def __init__(self, input_size, embed_size, hidden_size, n_layers=1, dropout=0.5):
         super(EncoderRNN, self).__init__()
@@ -49,7 +193,9 @@ class Attn(nn.Module):
         return F.softmax(attn_energies).unsqueeze(1) # normalize with softmax
 
     def score(self, encoder_outputs):
-        energy = self.attn(encoder_outputs)
+
+        energy = self.attn(encoder_outputs) #[B,T,H]
+        #should we apply an activation function to signals from hidden layer?
         energy = energy.transpose(2,1) # [B*H*T]
         v = self.v.repeat(encoder_outputs.data.shape[0],1).unsqueeze(1) #[B*1*H]
         energy = torch.bmm(v, energy) # [B*1*T]
@@ -83,3 +229,4 @@ class AttnDecoder(nn.Module):
         context = attn_weights.bmm(encoder_outputs.transpose(0, 1))  # (B,1,V)
         context = context.transpose(0, 1)  # (1,B,V)
         return self.ff(context)
+"""
--- wordsim.py
+++ wordsim.py
@@ -1,7 +1,256 @@
-import random
+#https://colab.research.google.com/drive/1jmtmD5JDRfWA3jpxjXIcsuh2uWrBr8RV
 
 import torch
+from torch import nn
+if torch.cuda.is_available(): import torch.cuda as t
+else: import torch as t
+import torch.nn.functional as F
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+import math, random
+
+
+# parameters
+
+
+PHONEME_INVENTORY_SIZE = 10 + 1
+SENTINEL_ID = PHONEME_INVENTORY_SIZE - 1
+VOCABULARY_SIZE = 100
+MAX_WORD_LENGTH = 5
+
+SENTINEL = object()
+
+PHONEMES = range(PHONEME_INVENTORY_SIZE-1) #exclude sentinel
+
+input_size = PHONEME_INVENTORY_SIZE
+embed_size = 10
+hidden_size = 100
+g_size = VOCABULARY_SIZE
+n_layers = 1 #EncoderLSTM
+dropout = 0.5
+learning_rate = 1e-3
+
+batch_size = 10 #training
+
+
+#Helper functions
+
+def one_hot(integers, k):
+  to_return = t.FloatTensor(len(integers),k).fill_(0)
+  to_return[range(len(integers)), integers] = 1
+  return to_return
+
+def idx_one_hot(one_hots, dim = 1):
+  '''
+  :param one_hots:
+    batch 1 hots in shape of [B,K]
+  '''
+  return [t.item() for t in torch.argmax(one_hots, dim)]
+
+def update_so_far(batch_a:list, current_batch_meanings_idx:list, so_far:dict, completed:dict = None):
+  #updating so_far excluding completed sequences
+
+  new_batch_meanings_idx = []
+  
+  for i in range(len(current_batch_meanings_idx)):
+    g_idx = current_batch_meanings_idx[i]
+    a = batch_a[i]
+    if a == SENTINEL_ID:
+      if(completed != None):
+        completed[g_idx] = so_far[g_idx] #save completed sequence
+      del so_far[g_idx]
+    else:
+      so_far[g_idx] += [a] #update sequences excluding completed
+      new_batch_meanings_idx.append(g_idx)
+      
+  return new_batch_meanings_idx
+
+def generate_random_word():
+    length = random.choice(range(1, MAX_WORD_LENGTH+1))
+    word = [random.choice(PHONEMES) for _ in range(length)]
+    return word
+
+def generate_random_vocabulary():
+    words = [generate_random_word() for _ in range(VOCABULARY_SIZE)]
+    return {i:word for i, word in enumerate(words)}
+
+# Have the agent forward-sample a sequence
+def agent_generate_word(batch_meanings, one_of_k_length,  encoder, attn, attn_decoder, sample = True):
+  '''
+  :param batch_meanings:
+      one hot encodings of meanings in shape [B,V] where V is vocabulary size
+  :param one_of_k_length:
+      used to generate one hot encodings of meanings k = V - vocabulary size 
+  '''
+  completed = {} #stores completed sequences as they are taken out of so_far
+
+  batch_meaning_idx = idx_one_hot(batch_meanings)
+  so_far = {g_idx:[] for g_idx in batch_meaning_idx} #stores seq as they are being generated
+  
+  current_batch_meanings_idx = [g_idx for g_idx in batch_meaning_idx] #updated by a == SENTINEL_ID
+  current_batch_meaning = one_hot(current_batch_meanings_idx, one_of_k_length)
+  
+  while len(current_batch_meanings_idx) > 0:
+    batch_g = t.FloatTensor(current_batch_meaning) #[B,G]
+    
+    input_seq = t.LongTensor([so_far[g_idx] for g_idx in current_batch_meanings_idx]).transpose(1,0) #[B,t].T ==> [t,B]
+
+    #encoder
+    outputs, _hidden = encoder(input_seq, None)
+
+    #policy
+    attn_weights = attn(outputs, batch_g)
+    a_distro = attn_decoder(outputs, attn_weights, batch_g) #[B,P]
+    a_distro = torch.distributions.Categorical(a_distro)
+    if(sample):
+      batch_a = a_distro.sample()
+    else:
+      batch_a = torch.argmax(a_distro.probs, 1)
+    
+    #update so_far & current batch idx
+    current_batch_meanings_idx = update_so_far(batch_a, current_batch_meanings_idx, so_far, completed) #updates so_far and returns new_batch_meanings
+    current_batch_meaning = one_hot(current_batch_meanings_idx, one_of_k_length)
+    
+  return completed
+
+# Get the log probability that the agent would produce a sequence
+def score_sequence(batch_meanings, batch_targets, encoder, attn, attn_decoder):
+    '''
+    :param batch_meanings:
+      one hot encodings of meanings in shape [B,V] where V is vocabulary size
+    :param batch_targets:
+      batch target sequences corresponding to batch meanings in shape [B,(T)] with various sequence lengths
+    
+    current_batch will keep track of sequences still in production, function ends when all seqeuences have been produced
+    '''
+    batch_size = len(batch_meanings)
+    
+    scores = t.FloatTensor([0.0 for _ in range(batch_size)])
+    
+    t_step = 0
+    
+    current_batch = [i for i in range(batch_size) if t_step < len(batch_targets[i])]
+    targets = [batch_targets[i][t_step] for i in current_batch] #[B]
+    input_seq = [batch_targets[i][:t_step] for i in current_batch] #[B,t]
+    
+    
+    while len(current_batch) > 0:
+      batch_g = torch.stack([batch_meanings[i] for i in current_batch]) #[B,G]
+      
+      outputs, _hidden = encoder(t.LongTensor(input_seq).transpose(1,0), None)
+      attn_weights = attn(outputs, batch_g)
+      a_distro = attn_decoder(outputs, attn_weights, batch_g) #[B,P] probability distribution of phonemes
+      n_log_p = -torch.log(a_distro) #cross entropy loss
+      scores[current_batch] += n_log_p[range(len(current_batch)),targets] #+= [B]
+      
+      t_step += 1
+      current_batch = [i for i in range(batch_size) if t_step < len(batch_targets[i])]
+      targets = [batch_targets[i][t_step] for i in current_batch] #[B]
+      input_seq = [batch_targets[i][:t_step] for i in current_batch] #[B,t]
+      
+    return scores
+
+def train(vocabulary,  encoder, attn, attn_decoder, opt, batch_size=5, num_epochs=10):
+    '''
+    :param vocabulary:
+      dict{meaning:sequence}
+    :param encoder:
+      EncoderRNN transforming sequence into T meaning representations (T(seq len) hidden states from RNN)
+    :param attn:
+      Attn 1 hidden layer feedforward neural net computing attention energies from T meaning representations
+    :attn_decoder:
+      Attn_decoder computes context (attention*encoder_outputs), forward into neural net to produce probability distribution of actions(phonemes)
+    '''
+    params = list(encoder.parameters()) + list(attn.parameters()) + list(attn_decoder.parameters())
+    
+    meanings = list(vocabulary.keys()) #[V]
+    
+    pos = 0
+    for epoch in range(num_epochs):
+        scores = t.FloatTensor(batch_size).fill_(0)
+        opt.zero_grad()
+        
+        batch_meanings = one_hot(meanings[pos:min(pos+batch_size, len(meanings))], len(vocabulary)) #[B,V]
+        
+        pos+=batch_size
+        if(pos >= len(meanings)):
+          pos = 0
+        
+        batch_targets = [vocabulary[g_idx]+[SENTINEL_ID] for g_idx in idx_one_hot(batch_meanings)]
+        
+        scores = score_sequence(batch_meanings, batch_targets, encoder, attn, attn_decoder)
+        loss = scores.mean()
+        loss.backward()
+        
+        #nn.utils.clip_grad_norm_(params, 5, norm_type=2) #clip to prevent exploding gradient
+        
+        opt.step()
+        
+        if (epoch % 100 == 0):
+          print(f"Epoch {epoch}, loss: {loss}")
+          
+    print(f"Epoch {epoch}, loss: {loss}")
+
+
+def main():
+    #init models
+    encoder = EncoderRNN(input_size, embed_size, hidden_size, n_layers, dropout, bidirectional = True).to(device)
+    attn = Attn(None, hidden_size, g_size = g_size).to(device)
+    attn_decoder = AttnDecoder([hidden_size,hidden_size,PHONEME_INVENTORY_SIZE], g_size, activation_fn = F.relu).to(device) #should we add more layers/make them bigger?
+    params = list(encoder.parameters()) + list(attn.parameters()) + list(attn_decoder.parameters())
+    optimizer = torch.optim.Adam(params, lr = learning_rate, weight_decay = .0001)
+
+    #init vocabulary
+    vocabulary = generate_random_vocabulary()
+    meanings = list(vocabulary.keys())
+
+    # #sampling agent
+    # batch_g_idx = [random.randint(0,len(vocabulary)-1) for _ in range(5)]
+    # batch_g = one_hot(batch_g_idx, len(vocabulary))
+
+    # target_as_str = "\n\t".join([str((i,vocabulary[i])) for i in batch_g_idx])
+    # print(f"Target: \n\t{target_as_str}")
+
+    # produced = agent_generate_word(batch_g, len(vocabulary), encoder, attn, attn_decoder, sample = True)
+    # produced_as_str = "\n\t".join([str((i,t.LongTensor(produced[i]))) for i in batch_g_idx])
+    # print(f"Produced: \n\t{produced_as_str}")
+
+    #training
+    import time
+    n_epochs = 1000
+
+
+    start_time = time.time()
+    train(vocabulary,  encoder, attn, attn_decoder, optimizer, batch_size=batch_size, num_epochs=n_epochs)
+    end_time = time.time()
+
+    print(f"{n_epochs} epochs in {end_time-start_time} seconds")
+
+    #evaluation
+    batch_g_idx = list(range(len(vocabulary)))
+    batch_g = one_hot(batch_g_idx, len(vocabulary))
+
+    incorrects = []
+    corrects = []
+
+    produced = agent_generate_word(batch_g, len(vocabulary), encoder, attn, attn_decoder, sample = True)
+    produced = {g_idx:produced[g_idx] for g_idx in batch_g_idx}
+
+    for g_idx in batch_g_idx:
+      if(len(vocabulary[g_idx]) == len(produced[g_idx]) and torch.all(torch.eq(t.FloatTensor(vocabulary[g_idx]),  t.FloatTensor(produced[g_idx])))):
+        corrects.append( (g_idx, vocabulary[g_idx], t.FloatTensor(produced[g_idx])) )
+      else:
+        incorrects.append( (g_idx, vocabulary[g_idx], t.FloatTensor(produced[g_idx])) )
+        
+    print(f"Num Correct: {len(corrects)}\nNum Incorrect: {len(incorrects)}")
+    #print(incorrects)
+
+
+if __name__ == '__main__':
+    main()
+
+"""
 PHONEME_INVENTORY_SIZE = 10
 VOCABULARY_SIZE = 100
 MAX_WORD_LENGTH = 5
@@ -40,8 +289,13 @@ def score_sequence(g, target, policy, encoder):
     score = 0.0
     for a in target:
         m = encoder(so_far)
-        score = policy(m, g)[a] # log p(a|m,g)
-        so_far.append(v)
+
+        #we add to score because log p(a1,a2,...|m_0,g) = Σ(log p(a_i|m_0,g))
+        #   but are we getting p(a_i|m_0,g) or p(a_i|m_i,g) from policy(m,g)[a]?
+        #       are they equivalent?
+        score += policy(m, g)[a] # log p(a|m,g) 
+
+        so_far.append(a)
     return score
 
 # Best loss is cross-entropy. For each true sequence, what is the log probability
@@ -57,7 +311,7 @@ def train(policy, encoder, vocabulary, batch_size=1, num_epochs=10000):
         loss = scores.mean()
         loss.backward()
         opt.step()
-        
+"""
         
 
         
